4c56af6e7bb093832b8001c19874d67f50ca65e7
ewoudje <ewoud.dierickx@hotmail.be>
2017-11-14 20:54:14 +0100

    Revert "Revert "Initial Commit""
    This reverts commit 752c4d2c4a4d74e77177d7308a4cd6a4aa4a7a53.

---
+++ Assets/CameraClicker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraClicker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)){ // if left button pressed...
+			Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit))
+			{
+				RenderCube renderCube = hit.transform.gameObject.GetComponent<RenderCube>();
+				if (renderCube != null)
+				{
+					CubeHandler.SelectCube(renderCube.id);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 6.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    private float turner;
+    private float looker;
+    private CharacterController controller;
+    public float sensitivity;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // is the controller on the ground?
+
+        //Feed moveDirection with input.
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        //Multiply it by speed.
+        moveDirection *= speed;
+        //Jumping
+        if (Input.GetButton("Jump"))
+            moveDirection.y = speed;
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+            moveDirection.y = -speed;
+
+        if (Input.GetMouseButton(1))
+        {
+            turner = Input.GetAxis("Mouse X") * sensitivity;
+            looker = -Input.GetAxis("Mouse Y") * sensitivity;
+            if (turner != 0)
+            {
+                //Code for action on mouse moving right
+                transform.eulerAngles += new Vector3(0, turner, 0);
+            }
+            if (looker != 0)
+            {
+                //Code for action on mouse moving right
+                transform.eulerAngles += new Vector3(looker, 0, 0);
+            }
+        }
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
